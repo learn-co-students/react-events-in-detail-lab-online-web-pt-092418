9cee7383e40a77ac3ae37f9209aeb732676752fd
github username <github email address>
2019-06-13 15:35:27 -0400

    coordinated button is working

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,20 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+export default class CoordinatesButton extends React.Component {
+
+  callback = (event) => {
+    let x = event.clientX;
+    let y = event.clientY;
+    this.props.onReceiveCoordinates([x, y])
+  }
+
+  render () {
+    return (
+      <div>
+        <button onClick={this.callback}>
+          Coordinates Button
+        </button>
+      </div>
+    )
+  }
+}
